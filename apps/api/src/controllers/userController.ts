90cfa5b076c3a021d920afec8b08cc23be0b325d
malindugurunada1BT <malindu.gurunada@1billiontech.com>
2026-06-22 13:51:09 +0530

    change tsconfig files to nodenext

--- apps/api/src/controllers/userController.ts
+++ apps/api/src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import UserService from '../services/userService';
+import UserService from '../services/userService.js';
 
 const getAll = async (_req: Request, res: Response) => {
   const users = await UserService.getAll();
--- apps/api/src/services/userService.ts
+++ apps/api/src/services/userService.ts
@@ -1,4 +1,4 @@
-import UserRepository from '../repositories/userRepository';
+import UserRepository from '../repositories/userRepository.js';
 
 const getAll = async () => {
   return UserRepository.getAll();
