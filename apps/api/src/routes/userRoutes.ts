e17ecadb5e4c9b7de1885418bed3497b0b2d5ead
chathurikasandamali <chathurika.sandamali@1billiontech.com>
2026-06-23 12:23:00 +0530

    Configured DB connection

--- apps/api/src/routes/userRoutes.ts
+++ apps/api/src/routes/userRoutes.ts
@@ -3,7 +3,7 @@ import UserController from '../controllers/userController.js';
 
 const router = Router();
 
-router.get('/', UserController.getAll);
+router.get('/getAll', UserController.getAll);
 router.post('/', UserController.create);
 
 export default router;
