2cf1f72eeaf334a1c5a1941da23c193261cad78d
omllnanayakkara <lahiru.nanayakkara@1billiontech.com>
2026-06-27 03:29:20 +0530

    FA-01-neon-auth-with-google-sso-v2

--- apps/web/src/lib/auth/server.ts
+++ apps/web/src/lib/auth/server.ts
@@ -2,10 +2,9 @@
 import { createNeonAuth } from '@neondatabase/auth/next/server';
 
 export const auth = createNeonAuth({
-  baseUrl: process.env.NEON_AUTH_BASE_URL!,
+  baseUrl: process.env.VERCEL_NEON_AUTH_BASE_URL!,
   cookies: {
-    secret: process.env.NEON_AUTH_COOKIE_SECRET || "my-secret-at-least-32-characters-long",
-    domain: process.env.NEON_AUTH_COOKIE_DOMAIN!,
+    secret: process.env.VERCEL_NEON_AUTH_COOKIE_SECRET || "my-secret-at-least-32-characters-long",
     sessionDataTtl: 300, // 7 days
   },
   // logLevel: 'silent',
